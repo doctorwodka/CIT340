258eec77d5a56bcad3753ee8ce73ce5bc74467c6
doctorwodka <jordanmjones75@gmail.com>
2022-11-15 15:51:34 -0500

    Scenes and platforms
    - added scenes
    - added cave sprites
    - added some player functionality
    - added ability to escape with escape key

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void GoBacky()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Exit()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+            Application.Quit();
+    #endif
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 10;
+    public float jumpPower = 10;
+
+    float horizontalMovement;
+    bool isJumping = false;
+
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer sr;
+
+    static Player playerReference;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+        if (playerReference == null)
+        {
+            playerReference = FindObjectOfType<Player>();
+
+        }
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetAxis("Cancel") == 1)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+        horizontalMovement = Input.GetAxis("Horizontal"); //Left + right
+        isJumping = Input.GetAxis("Jump") > 0 ? true : false; //Jumping
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
+
+        if (isJumping)
+        {
+            Vector3 feetPosition = transform.GetChild(0).position;
+
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(feetPosition, 0.25f);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].gameObject == gameObject)
+                    continue;
+
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+                rb.AddForce(Vector2.up * jumpPower);
+                break;
+            }
+
+        }
+
+        if (rb.velocity.magnitude > 0.05f)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+
+        if (rb.velocity.x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (rb.velocity.x > 0)
+        {
+            sr.flipX = false;
+        }
+
+    }
+
+}
